92e4cd6d7626adc1e71d6bf350e847db65e4a371
Rakibul <mdrakibulhassan2425@gmail.com>
2024-03-30 12:26:47 +0600

    add sign out functionality

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,43 @@
-import { getAuth,GoogleAuthProvider,signInWithPopup  } from "firebase/auth";
+import { getAuth,GoogleAuthProvider,signInWithPopup, signOut  } from "firebase/auth";
 import app from "../../firebase/fiebase.init";
+import { useState } from "react";
 
 const Login = () => {
+    const [user, setUser] = useState(null);
     const auth = getAuth(app)
     const provider = new GoogleAuthProvider();
     const signInWithGooglePopUp = () => {
         signInWithPopup(auth, provider)
             .then(result => {
-                const user = result.user;
-                console.log(user,)
+                const loginedUser = result.user;
+                console.log(loginedUser);
+                setUser(loginedUser)
             })
         .catch(e => console.log(e))
     }
+    const logOut = () => {
+        signOut(auth)
+            .then(result => setUser(null))
+            .catch(e => console.log(e))
+    }
     return (
-        <div>
-            <button onClick={signInWithGooglePopUp}>Sign in With Google</button>
+        <div className="my-5">
+            {
+                user ? 
+                    <button
+                        onClick={logOut}
+                        className="bg-orange-500 px-4 py-2 rounded-xl text-white font-bold"
+                    >LogOut</button> : <button onClick={signInWithGooglePopUp}
+                    className="bg-lime-600 px-4 py-2 rounded-xl text-white font-bold"
+                    >Sign in With Google</button>
+            }
+            
+            
+            <div>
+                <h1>User Name : {user?.displayName}</h1>
+                <h1>User Email : {user?.email}</h1>
+                <img src={user?.photoURL} alt="" />
+            </div>
         </div>
     )
 }
