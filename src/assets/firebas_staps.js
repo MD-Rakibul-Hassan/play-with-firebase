052ca812663cc1480808d5a004825f9278516fcd
Rakibul <mdrakibulhassan2425@gmail.com>
2024-03-30 14:05:26 +0600

    github integration added

--- src/assets/firebas_staps.js
+++ src/assets/firebas_staps.js
@@ -1,14 +1,20 @@
 /*
+**********Inital setup**********
+
 1 . visit : console.firebase.google.com;
 2 . create project (skip google analytics)
 3 . register app (create config)
 4 . install firebase : npm i firebase
 5 . add config file to project
 6 . Danger! : Do not publish or firebase config to public by pushing those to github 
+***********Integration***********
+
 7 . Visit : firebase > Go to Docs > Build > Authentication > Web > Get started
 8 . export app form the firebase.init.js file 
 9 . import getAut form firebase/auth and app
 10 . make const auth = getAuth(app)
+***************Provider setup************
+
 11 . import googleAuthProvider and make a provider
 12 . signInwithPopUp and pass auth and provider
 13 . activate sign-in method (like : google facebook, github etc)
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,30 @@
-import { getAuth,GoogleAuthProvider,signInWithPopup, signOut  } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider } from "firebase/auth";
 import app from "../../firebase/fiebase.init";
 import { useState } from "react";
 
 const Login = () => {
     const [user, setUser] = useState(null);
     const auth = getAuth(app)
-    const provider = new GoogleAuthProvider();
+    const providerGoogle = new GoogleAuthProvider();
+    const providerGithub = new GithubAuthProvider();
     const signInWithGooglePopUp = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, providerGoogle)
             .then(result => {
                 const loginedUser = result.user;
                 console.log(loginedUser);
                 setUser(loginedUser)
             })
         .catch(e => console.log(e))
+    }
+    const signInWithGithub = () => {
+        signInWithPopup(auth, providerGithub)
+            .then(result => {
+                const loginedUser = result.user;
+                setUser(loginedUser);
+                console.log(loginedUser)
+            })
+            .catch(e => console.log(e.message))
     }
     const logOut = () => {
         signOut(auth)
@@ -27,9 +38,16 @@ const Login = () => {
                     <button
                         onClick={logOut}
                         className="bg-orange-500 px-4 py-2 rounded-xl text-white font-bold"
-                    >LogOut</button> : <button onClick={signInWithGooglePopUp}
-                    className="bg-lime-600 px-4 py-2 rounded-xl text-white font-bold"
-                    >Sign in With Google</button>
+                    >LogOut</button> : <div>
+                        <button onClick={signInWithGooglePopUp}
+                            className="bg-lime-600 px-4 py-2 rounded-xl text-white font-bold"
+                            >Sign in With Google
+                        </button>
+                        <button onClick={signInWithGithub}
+                            className="bg-lime-600 px-4 py-2 rounded-xl text-white font-bold"
+                            >Sign in With Github
+                        </button>
+                    </div>
             }
             
             
